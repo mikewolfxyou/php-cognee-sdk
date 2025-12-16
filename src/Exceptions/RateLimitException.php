a6463e647f13df75ecfb589a412638c7060387f8
Zhiguo Zhang <zhiguo.zhang@real-digital.de>
2025-12-16 10:14:10 +0100

    Fix typo in RateLimitException doc comment

--- src/Exceptions/RateLimitException.php
+++ src/Exceptions/RateLimitException.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Cognee\Exceptions;
 
 /**
- * Exception thrown when rate limit is exceeded (429).
+ * Exception thrown when the rate limit is exceeded (429).
  */
 class RateLimitException extends CogneeException
 {
